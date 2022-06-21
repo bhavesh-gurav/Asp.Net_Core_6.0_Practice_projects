9df88239059a13d6f48795e482e0bc1d533b2ed6
bhavesh <bhavesh.gurav.in@gmail.com>
2022-06-21 17:46:49 +0530

    First Commit

---
+++ MyWebApp/MyWebApp/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyWebApp.Data;
+using MyWebApp.Models;
+
+namespace MyWebApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _context.Categories;
+
+            return View(categories);
+        }
+    }
+}

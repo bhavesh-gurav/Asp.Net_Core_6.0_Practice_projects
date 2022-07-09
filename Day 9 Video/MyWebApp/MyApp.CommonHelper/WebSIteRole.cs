a46ffe818a37e71edf3f3f4a617fe77a5d923505
bhavesh <bhavesh.gurav.in@gmail.com>
2022-07-09 17:24:02 +0530

    Video 9 Done

---
+++ Day 9 Video/MyWebApp/MyApp.CommonHelper/WebSIteRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp.CommonHelper
+{
+    public static class WebSIteRole
+    {
+        //Adding roles in new class and also create new common helper class library.
+        public const string Role_User = "Customer";
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+    }
+}
---
+++ Day 9 Video/MyWebApp/MyWebApp/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,130 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyApp.DataAccessLayer;
+using MyApp.DataAccessLayer.Infrastructure.IRepository;
+using MyApp.Models;
+using MyApp.Models.ViewModels;
+
+namespace MyWebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        
+        private IUnitOfWork _unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            CategoryVM categoryVM = new CategoryVM();
+            categoryVM.categories = _unitOfWork.Category.GetAll();
+            return View(categoryVM);
+        }
+
+        //[HttpGet]
+        //public IActionResult Create()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Create(Category category)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        _unitOfWork.Category.Add(category);
+        //        _unitOfWork.Save();
+        //        TempData["success"] = "Category Created Done...!";
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(category);
+
+        //}
+
+
+
+        [HttpGet]
+        public IActionResult CreateUpdate(int? id)
+        {
+            CategoryVM vm = new CategoryVM();
+            if (id == null || id == 0)
+            {
+                return View(vm);
+            }
+            else
+            {
+                vm.Category = _unitOfWork.Category.GetT(x => x.Id == id);
+                if (vm.Category == null)
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(vm);
+                }
+                
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult CreateUpdate(CategoryVM vm)
+        {
+            if (ModelState.IsValid)
+            {
+                if(vm.Category.Id == 0)
+                {
+                    _unitOfWork.Category.Add(vm.Category);
+                    TempData["success"] = "Category Created Done...!";
+                }
+                else
+                {
+                    _unitOfWork.Category.Update(vm.Category);
+                    TempData["success"] = "Category Updated Done...!";
+                }
+                
+                _unitOfWork.Save();
+                
+                //return RedirectToAction("Index");
+            }
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _unitOfWork.Category.GetT(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeleteData(int? id)
+        {
+            var category = _unitOfWork.Category.GetT(x => x.Id == id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Category.Delete(category);
+            _unitOfWork.Save();
+            TempData["success"] = "Category Deleted Done...!";
+            return RedirectToAction("Index");
+
+        }
+
+    }
+}

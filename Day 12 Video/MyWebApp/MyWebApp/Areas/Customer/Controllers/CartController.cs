7a9888731d6b94d4e218330cac8429c25550cbf4
bhavesh <bhavesh.gurav.in@gmail.com>
2022-07-14 14:21:45 +0530

    Video 12 Done

---
+++ Day 12 Video/MyWebApp/MyWebApp/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyApp.DataAccessLayer.Infrastructure.IRepository;
+using MyApp.Models.ViewModels;
+using System.Security.Claims;
+
+namespace MyWebApp.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    [Authorize]
+    public class CartController : Controller
+    {
+
+        private IUnitOfWork _unitOfWork;
+        public CartVM itemList { get; set; }
+        public CartController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            var claimIdentity = (ClaimsIdentity)User.Identity;
+            var claims = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            itemList = new CartVM()
+            {
+                ListOfCart = _unitOfWork.Cart.GetAll(x => x.ApplicationUserId == claims.Value, includeProperties: "Product")
+            };
+
+            foreach (var item in itemList.ListOfCart)
+            {
+                itemList.Total += (item.Product.Price * item.Count);
+            }
+
+            return View(itemList);
+        }
+
+        public IActionResult plus(int id)
+        {
+            var cart = _unitOfWork.Cart.GetT(x =>x.Id == id);
+            _unitOfWork.Cart.IncrementCartItem(cart, 1);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult minus(int id)
+        {
+            var cart = _unitOfWork.Cart.GetT(x => x.Id == id);
+            if (cart.Count <= 1)
+            {
+                _unitOfWork.Cart.Delete(cart);
+            }
+            else
+            {
+                _unitOfWork.Cart.DecrementCartItem(cart, 1);
+            }
+            
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+        public IActionResult delete(int id)
+        {
+            var cart = _unitOfWork.Cart.GetT(x => x.Id == id);
+            _unitOfWork.Cart.Delete(cart);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}

72ae2275bde377451ba915afcce48a332cd1c7c9
bhavesh <bhavesh.gurav.in@gmail.com>
2022-07-20 18:23:43 +0530

    Day 20 Video 21

---
+++ Day 20 Video 21/MyWebApp/MyWebApp/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyApp.CommonHelper;
+using MyApp.DataAccessLayer.Infrastructure.IRepository;
+using MyApp.Models;
+using MyApp.Models.ViewModels;
+using System.Security.Claims;
+
+namespace MyWebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        #region APICALL
+        public IActionResult AllOrders(string status)
+        {
+            IEnumerable<OrderHeader> orderHeaders;
+            
+            if (User.IsInRole("Admin") || User.IsInRole("Employee"))
+            {
+                orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            }
+            else
+            {
+                var claimIdentity = (ClaimsIdentity)User.Identity;
+                var claims = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                orderHeaders = _unitOfWork.OrderHeader.GetAll(x => x.ApplicationUserId == claims.Value);
+            }
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaders = orderHeaders.Where(x => x.PaymentStatus == PaymentStatus.StatusPending);
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(x => x.PaymentStatus == PaymentStatus.StatusApproved);
+                    break;
+
+                default:
+                    break;
+            }
+            return Json(new { data = orderHeaders });
+        }
+        #endregion
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult OrderDetails(int id)
+        {
+            OrderVM orderVM = new OrderVM()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.GetT(x => x.Id == id,
+                includeProperties: "ApplicationUser"),
+                OrderDatail = _unitOfWork.OrderDetail.GetAll(x => x.Id == id, includeProperties: "Product")
+            };
+
+            return View(orderVM);
+        }
+
+        [HttpPost]
+        public IActionResult OrderDetails(OrderVM vm)
+        {
+            var orderHeader = _unitOfWork.OrderHeader.GetT(x => x.Id == vm.OrderHeader.Id);
+            orderHeader.Name = vm.OrderHeader.Name;
+            orderHeader.Phone = vm.OrderHeader.Phone;
+            orderHeader.Address = vm.OrderHeader.Address;
+            orderHeader.City = vm.OrderHeader.City;
+            orderHeader.State = vm.OrderHeader.State;
+            orderHeader.PostalCode = vm.OrderHeader.PostalCode;
+            if (vm.OrderHeader.Carrier != null)
+            {
+                orderHeader.Carrier = vm.OrderHeader.Carrier;
+            }
+            if (vm.OrderHeader.TrackingNumber != null)
+            {
+                orderHeader.TrackingNumber = vm.OrderHeader.TrackingNumber;
+            }
+            _unitOfWork.OrderHeader.Update(orderHeader);
+            _unitOfWork.Save();
+            TempData["success"] = "Info Updated";
+            return RedirectToAction("OrderDetails", "Order", new {id = vm.OrderHeader.Id});
+           
+        }
+
+        public IActionResult InProcess(OrderVM vm)
+        {
+            _unitOfWork.OrderHeader.UpdateStatus(vm.OrderHeader.Id, OrderStatus.StatusInProcess);
+            _unitOfWork.Save();
+            TempData["success"] = "Order Status Updated InProcess";
+            return RedirectToAction("OrderDetails", "Order", new { id = vm.OrderHeader.Id });
+        }
+
+        public IActionResult Shipped(OrderVM vm)
+        {
+            var orderHeader = _unitOfWork.OrderHeader.GetT(x => x.Id == vm.OrderHeader.Id);
+            orderHeader.Carrier = vm.OrderHeader.Carrier;
+            orderHeader.TrackingNumber = vm.OrderHeader.TrackingNumber;
+            orderHeader.OrderStatus = OrderStatus.StatusShipped;
+            orderHeader.DateOfShipping = DateTime.Now;
+
+            _unitOfWork.OrderHeader.Update(orderHeader);
+            _unitOfWork.Save();
+            TempData["success"] = "Order Status Updated Shipped";
+            return RedirectToAction("OrderDetails", "Order", new { id = vm.OrderHeader.Id });
+        }
+
+    }
+}

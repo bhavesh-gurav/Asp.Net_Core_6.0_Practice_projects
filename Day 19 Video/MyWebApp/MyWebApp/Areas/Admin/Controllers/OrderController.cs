870c43c006febf88eef2ee5290affcc87e13c7d6
bhavesh <bhavesh.gurav.in@gmail.com>
2022-07-20 16:22:52 +0530

    Video 19 Done

---
+++ Day 19 Video/MyWebApp/MyWebApp/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyApp.CommonHelper;
+using MyApp.DataAccessLayer.Infrastructure.IRepository;
+using MyApp.Models;
+using MyApp.Models.ViewModels;
+using System.Security.Claims;
+
+namespace MyWebApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class OrderController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        #region APICALL
+        public IActionResult AllOrders(string status)
+        {
+            IEnumerable<OrderHeader> orderHeaders;
+            
+            if (User.IsInRole("Admin") || User.IsInRole("Employee"))
+            {
+                orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+            }
+            else
+            {
+                var claimIdentity = (ClaimsIdentity)User.Identity;
+                var claims = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+                orderHeaders = _unitOfWork.OrderHeader.GetAll(x => x.ApplicationUserId == claims.Value);
+            }
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaders = orderHeaders.Where(x => x.PaymentStatus == PaymentStatus.StatusPending);
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(x => x.PaymentStatus == PaymentStatus.StatusApproved);
+                    break;
+
+                default:
+                    break;
+            }
+            return Json(new { data = orderHeaders });
+        }
+        #endregion
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult OrderDetails(int id)
+        {
+            OrderVM orderVM = new OrderVM()
+            {
+                OrderHeader = _unitOfWork.OrderHeader.GetT(x => x.Id == id,
+                includeProperties: "ApplicationUser"),
+                OrderDatail = _unitOfWork.OrderDetail.GetAll(x => x.Id == id, includeProperties: "Product")
+            };
+
+            return View(orderVM);
+        }
+
+    }
+}
